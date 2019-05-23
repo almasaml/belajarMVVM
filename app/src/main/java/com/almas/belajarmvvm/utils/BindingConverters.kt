c17ef58d24066c26c5d4b1ae27f0ea225585bac9
Almas Amalia <almasamaliaazhar@gmail.com>
2019-05-23 10:34:27 +0700

    finishing app

--- app/src/main/java/com/almas/belajarmvvm/utils/BindingConverters.kt
+++ app/src/main/java/com/almas/belajarmvvm/utils/BindingConverters.kt
@@ -3,17 +3,19 @@ package com.almas.belajarmvvm.utils
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.almas.belajarmvvm.R
-import java.net.URL
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.request.RequestOptions
 
 class BindingConverters {
     companion object{
 
         @JvmStatic
-        @BindingAdapter("Load Image")
+        @BindingAdapter("loadImage")
         fun loadImage(imageView: ImageView, url: String) {
             Glide.with(imageView.context)
                 .load(url)
-                .apply(RequestOption()
+                .apply(RequestOptions()
                     .placeholder(R.drawable.sample_image)
                     .error(R.drawable.sample_image)
                     .format(DecodeFormat.PREFER_ARGB_8888))
